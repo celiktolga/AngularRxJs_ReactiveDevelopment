f85a34c4bbab0b46cb0b4d05c894760e34e69c3a
Tolga ÇELİK <tolga_ee_engineer@hotmail.com>
2023-04-08 00:19:53 +0100

    SUBJECT /behaviour subject

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -4,7 +4,7 @@ import { ProductCategory } from '../product-categories/product-category';
 
 import { Product } from './product';
 import { ProductService } from './product.service';
-import { EMPTY, Observable, catchError, filter, map, of } from 'rxjs';
+import { EMPTY, Observable, Subject, catchError, combineLatest, filter, map, of } from 'rxjs';
 import { ProductCategoryService } from '../product-categories/product-category.service';
 
 @Component({
@@ -16,16 +16,34 @@ export class ProductListComponent {
   pageTitle = 'Product List';
   errorMessage = '';
   //categories: ProductCategory[] = [];
-  selectedCategoryId = 1;
+  //selectedCategoryId = 1;
+  private categorySelectedSubject = new Subject<number>();
+  categoryselectedAction$ = this.categorySelectedSubject.asObservable();
 
   //products$: Observable<Product[]> | undefined;
-  products$ = this.productService.productsWithCategory$
+  /*products$ = this.productService.productsWithCategory$
     .pipe(
       catchError(err => {
         this.errorMessage = err
         return EMPTY;
       })
-    );
+    );*/
+
+  products$ = combineLatest([
+    this.productService.productsWithCategory$,
+    this.categoryselectedAction$
+  ])
+    .pipe(
+      map(([products, selectedCategoryId]) =>
+        products.filter(product =>
+          selectedCategoryId ? product.categoryId === selectedCategoryId : true
+        )),
+
+        catchError(err => {
+          this.errorMessage = err;
+          return EMPTY;
+        })
+    )
 
   categories$ = this.productCategoryService.productCategories$
       .pipe(
@@ -35,13 +53,13 @@ export class ProductListComponent {
         })
       )
 
-  productsSimpleFilter$ = this.productService.productsWithCategory$
+  /*productsSimpleFilter$ = this.productService.productsWithCategory$
     .pipe(
       map(products =>
         products.filter(product =>
           this.selectedCategoryId ? product.categoryId === this.selectedCategoryId : true //return all ıf there is no selecte category id
         ))
-      )
+      )*/
 
   constructor(private productService: ProductService,
               private productCategoryService: ProductCategoryService) { }
@@ -62,6 +80,7 @@ export class ProductListComponent {
 
   onSelected(categoryId: string): void {
     //console.log('Not yet implemented');
-    this.selectedCategoryId = +categoryId;
+    //this.selectedCategoryId = +categoryId;
+    this.categorySelectedSubject.next(+categoryId); //+ sign convert string to number
   }
 }
